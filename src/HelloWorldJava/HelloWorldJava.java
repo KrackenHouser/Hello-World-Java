8202b4f93b7c3ec3832e4a5987710a76b10c03ee
KrackenHouser <dawnsss23@proton.me>
2024-10-05 17:57:26 +0200

    Primer Commit

---
+++ src/HelloWorldJava/HelloWorldJava.java
@@ -0,0 +1,13 @@
+package HelloWorldJava;
+
+public class HelloWorldJava {
+
+	public static void main(String[] args) {
+
+		for(int i = 1; i <= 1; i++) {
+			
+			System.out.println("Hola Mundo" );
+		}
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module HElloWorld {
+}
